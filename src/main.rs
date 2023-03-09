1e90e90452ecba0c9fb9c45f5dda405385c22c72
Rakhim Davletkaliyev <rakhim.davletkaliyev@meetiqm.com>
2023-03-09 17:47:09 +0200

    templates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,85 @@
-use std::fs;
-use std::ffi;
-use fs_extra;
-use comrak::{markdown_to_html, ComrakOptions};
 use clap::Parser;
+use comrak::{markdown_to_html, ComrakOptions};
 use devserver_lib;
-use minijinja::{context, Environment};
+use fs_extra;
+use minijinja::{context, AutoEscape, Environment};
+use serde::Serialize;
+use std::ffi;
+use std::fs;
 
+const TEMPLATE_SINGLE: &str = r#"<!doctype html>
+<html>
+<head>
+    <title>{{ title }}</title>
+    <link rel="stylesheet" href="/style.css">
+    <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template">
+</head>
+<body>
+    <main>
+    {{ body }}
+    </main>
+</body>
+</html>"#;
+
+const TEMPLATE_INDEX: &str = r#"<!doctype html>
+<html>
+<head>
+    <title>{{ title }}</title>
+    <link rel="stylesheet" href="/style.css">
+    <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template">
+</head>
+<body>
+    <main>
+    <ul class="nav">
+    {% for item in items %}
+      <li><a href="{{ item.path }}">{{ item.title }}</a></li>
+    {% endfor %}
+    </ul>
+    </main>
+</body>
+</html>"#;
+
+const TEMPLATE_STYLE: &str = r#"
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  line-height: 1.4;
+  font-size: 16px;
+  margin: 0;
+  background-image: radial-gradient(#53575712 1px, transparent 0);
+  background-size: 8px 8px;
+  background-color: #c0c7c842;
+  border-top: 1rem solid #0000A8;
+}
+
+main {
+  margin: 50px auto;
+  max-width: 650px;
+  padding: 16px;
+}
+
+img {
+  max-width: 100%;
+}
+
+div.item {
+  display: flex;
+  margin-bottom: 0.5em;
+}
+
+div.date {
+  margin-right: 1em;
+}
+
+@media screen and (max-width: 600px) {
+  main {
+    margin: 10px auto;
+    max-width: 100%;
+  }
+}"#;
+
+#[derive(Serialize)]
 struct Item {
     title: String,
     path: String,
@@ -24,7 +98,7 @@ struct Args {
     serve: bool,
 
     #[arg(short, long)]
-    port: Option<u32>
+    port: Option<u32>,
 }
 
 fn sanitize_filename(filename: &String) -> String {
@@ -37,7 +111,8 @@ fn sanitize_filename(filename: &String) -> String {
             result.push('_');
         }
     }
-    return result.to_lowercase()
+    return result
+        .to_lowercase()
         .trim_start_matches('_')
         .trim_end_matches('_')
         .to_string();
@@ -55,58 +130,79 @@ fn main() -> std::io::Result<()> {
     options.extension.tagfilter = false;
     options.render.unsafe_ = true;
 
+    let mut template_env = Environment::new();
+    template_env.set_auto_escape_callback(|_name| AutoEscape::None);
+
+    template_env
+        .add_template("single.html", TEMPLATE_SINGLE)
+        .unwrap();
+
     for path in paths {
         let md_filepath = path.as_ref().unwrap().path(); //  blog/file?a.md
         let is_file = path.as_ref().unwrap().file_type().unwrap().is_file();
 
         if is_file && md_filepath.extension() == Some(ffi::OsStr::new("md")) {
             let md_filename = path.as_ref().unwrap().file_name().into_string().unwrap(); // file?a.md
-            let title = md_filename[..&md_filename.len()-3].to_owned(); // file?a
-            let md_filename_sanitized = sanitize_filename(&title);  // file_a
+            let title = md_filename[..&md_filename.len() - 3].to_owned(); // file?a
+            let md_filename_sanitized = sanitize_filename(&title); // file_a
 
-            let contents = fs::read_to_string(md_filepath)
-                .expect("Should have been able to read the file");
+            let contents =
+                fs::read_to_string(md_filepath).expect("Should have been able to read the file");
             let html = markdown_to_html(&contents, &options);
 
-            fs_extra::dir::create(format!("{}/{}", &args.output_dir, md_filename_sanitized), true)
-                .expect("Cannot create output dir");
+            fs_extra::dir::create(
+                format!("{}/{}", &args.output_dir, md_filename_sanitized),
+                true,
+            )
+            .expect("Cannot create output dir");
 
-            for line in contents.lines() {
-                if &line.len() > &3 && &line[..2] == "![" {
-                    let img_file_path = &line[4..&line.len()-1];
-                    println!("{}", format!("{}/attachments/{}", &args.input_dir, &img_file_path));
+            let c = contents.lines().count();
+            for line in contents.lines().enumerate() {
+                if line.0 == c-1 {
+                    println!("{}", c.to_string());
+                    println!("{}", &line.1);
+                }
+                if &line.1.len() > &3 && line.1.starts_with("![") {
+                    let img_file_path = &line.1[4..&line.1.len() - 1];
+                    println!(
+                        "{}",
+                        format!("{}/attachments/{}", &args.input_dir, &img_file_path)
+                    );
                     fs::copy(
                         format!("{}/attachments/{}", &args.input_dir, &img_file_path),
-                        format!("{}/{}/{}", &args.output_dir, &md_filename_sanitized, &img_file_path)
+                        format!(
+                            "{}/{}/{}",
+                            &args.output_dir, &md_filename_sanitized, &img_file_path
+                        ),
                     )?;
                 }
             }
 
+            let template = template_env.get_template("single.html").unwrap();
+            let result = template
+                .render(context!(title => "Rabla!", body => &html))
+                .unwrap();
+
             let path = format!("{}/{}/index.html", args.output_dir, md_filename_sanitized);
-            fs::write(path, &html).expect("Unable to write file");
+            fs::write(path, &result).expect("Unable to write file");
 
             let new_post = Item {
-                title,
-                path: md_filename_sanitized
+                title: title,
+                path: format!("{}/", md_filename_sanitized),
             };
             items.push(new_post);
         }
-
     }
 
-    // Write index.html
-    let mut env = Environment::new();
-    env.add_template("hello.txt", "Hello {{ name }}!").unwrap();
-    let template = env.get_template("hello.txt").unwrap();
+    // Write index.html & style.css
+    template_env.add_template("index.html", TEMPLATE_INDEX).unwrap();
+    let template = template_env.get_template("index.html").unwrap();
 
-    let mut index_html_list = Vec::new();
-    for item in &items {
-        index_html_list.push(format!("<li><a href=\"{}/\">{}</a></li>", item.path.clone(), item.title.clone()));
-    }
-    let joined = index_html_list.join("\n");
-    let result = template.render(context!(name => joined)).unwrap();
+    let result = template
+        .render(context!(title => "Rabla!", items => items))
+        .unwrap();
     fs::write("site/index.html", &result).expect("Unable to write file");
-
+    fs::write("site/style.css", TEMPLATE_STYLE).expect("Unable to write file");
 
     if args.serve {
         let mut port: u32 = 8090;
@@ -114,7 +210,7 @@ fn main() -> std::io::Result<()> {
             port = args.port.unwrap();
         }
         println!("\nServing at http://localhost:{}", port);
-        devserver_lib::run(&"localhost", port, &"site", true, "");
+        devserver_lib::run(&"localhost", port, &"site", false, "");
     }
 
     Ok(())
